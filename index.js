386e94495785584f0aaa345ba811d3b2f01bd554
Jessica Weinstein <jessicaweinstein@Jessicas-MacBook-Pro.local>
2020-04-05 13:58:14 -0400

    modal added to index.html, css and js documents, Timeout modal is working in js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 //   - the website should have a modal - a div that appears a sec or two after you launch the page; this modal can appear on any of the pages (could be your contact info for example);
 //   - the website should have a image carousel where you can press buttons and go left or right and see different photos;	
 
-
-
+//IMAGE SLIDER/CAOURSEL
 let leftButton = document.getElementById("leftButton")
 let rightButton = document.getElementById("rightButton")
 let images = document.getElementsByClassName("sliderPhotoContainer")
@@ -35,3 +34,32 @@ function moveRight() {
 	images [index].style.display = "block";
 
 }
+//END IMAGE SLIDER/CAOURSEL
+
+
+//START MODAL
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+window.onload = function(){ 
+setTimeout(function(){modal.style.display = "block";}, 3000);
+}
+
+// END MODEL
